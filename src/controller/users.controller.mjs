f2b9182279942bac8ad9a5b9dfca149af8ffb815
BreidyReyes <breidyreyesb1@gmail.com>
2025-05-27 19:45:48 -0500

    se termino de hacer la api de users hasta el momento creo

--- src/controller/users.controller.mjs
+++ src/controller/users.controller.mjs
@@ -2,33 +2,83 @@ import userModel from "../schemas/User.schema.mjs";
 
 const createUsers = async (req, res) => {
     const inputData = req.body;
-    const data = await userModel.create( inputData );
+    const data = await userModel.create(inputData);
 
-        try {
-        const registeredProduct = await userModelModel.create( inputData );
+    try {
+        const registeredProduct = await userModelModel.create(inputData);
 
-        console.log( registeredProduct );        // Imprime en la consula
-        res.send( registeredProduct );           // Enviando la respuesta al cliente
-    } 
+        console.log(registeredProduct);        // Imprime en la consula
+        res.send(registeredProduct);           // Enviando la respuesta al cliente
+    }
     catch (error) {
-        console.error( error );
-        res.send( 'Error: No se pudo registrar el producto' );
+        console.error(error);
+        res.send('Error: No se pudo registrar el producto');
     }
 }
 
-const getAllProducts = async (req, res) => {
+const getAllUsers = async (req, res) => {
 
     try {
         const data = await userModel.find({})
+        res.json(data);
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ msg: "error: no se pudo optener el resultado de productos" })
+    }
+}
+
+const getUsersById = async (req, res) => {
+    const UsersId = req.params.id;
+    try {
+        const data = await userModel.findById(UsersId);
+
+        if (!data) {
+            return res.json({ msg: "el producto no se encuentra resgistrado" })
+        }
+        res.json(data);
+    }
+    catch (error) {
+        console.error(error);
+        res.json({ msg: "error: no se pudo encontrar el producto" })
+    }
+}
+
+const removeUsersById = async (req, res) => {
+    try {
+    const productId = req.params.id;
+    const data = await userModel.findByIdAndDelete(productId);
+    if (!data) {
+        return res.json({ msg: "el producto no se encuentra resgistrado" })
+    }
+
+    res.json(data);
+    }
+
+    catch (error) {
+        console.error(error);
+        res.json({ msg: "error: no se pudo encontrar el producto" })
+    }
+}
+
+const updateUsersById = async (req, res) => {
+    try {
+    const usersId = req.params.id;
+    const inputData = req.body;
+    const data = await userModel.findByIdAndUpdate( usersId, inputData, {new:true}  );
+    
     res.json( data );
     }
     catch ( error ) {
-        console.error( error );
-        res.json ( {msg: "error: no se pudo optener el resultado de productos"})
+        console.error( error )
+        res.json({msg: "no se pudo actualizar el usuario"})
     }
 }
 
 export {
     createUsers,
-    getAllProducts
+    getAllUsers,
+    getUsersById,
+    removeUsersById,
+    updateUsersById
 }
--- src/routes/users.route.mjs
+++ src/routes/users.route.mjs
@@ -1,10 +1,13 @@
 import express from 'express';
 
-import { createUsers, getAllProducts} from '../controller/users.controller.mjs'
+import { createUsers, getAllUsers, getUsersById, removeUsersById, updateUsersById} from '../controller/users.controller.mjs'
 
 const router = express.Router();
 
 router.post('/api/users', createUsers);
-router.get("/api/users", getAllProducts)
+router.get("/api/users", getAllUsers)
+router.get("/api/users/:id", getUsersById)
+router.delete("/api/users/:id", removeUsersById);
+router.patch("/api/users/:id", updateUsersById);
 
 export default router;
