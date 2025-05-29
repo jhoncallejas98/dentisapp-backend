3a7cda9983b18045d2130c2bdc95b9ed79756147
BreidyReyes <breidyreyesb1@gmail.com>
2025-05-29 18:13:06 -0500

    nuevos cambios

--- src/routes/disponibilidad.router.mjs
+++ src/routes/disponibilidad.router.mjs
@@ -1,12 +1,14 @@
 import express from "express";
-import { createDiponibilidad } from "../controller/disponibilidad.controller.mjs";
+import { createDiponibilidad, getAllDisponibilidad, updatedisponibilidadById } from "../controller/disponibilidad.controller.mjs";
 
 
 const router = express.Router();
 
 //define las rutas para la entidad disponibilidad
 router.post("/api/disponibilidad",  createDiponibilidad );
+router.get("/api/disponibilidad",  getAllDisponibilidad );
 
+router.patch("/api/disponibilidad", updatedisponibilidadById)
 
 
 export default router;
