caa679bcca668c53219edfed16c0906bf6674c21
Jhon <jhoncallejas1998@hotmail.com>
2025-05-29 21:26:27 -0500

    terminado login parte 1

--- src/routes/auth.router.mjs
+++ src/routes/auth.router.mjs
@@ -1,7 +1,10 @@
 import express from 'express';
 import { createUser } from '../controller/users.controller.mjs';
+import { userLogin } from '../controller/auth.controller.mjs';
 
 const router = express.Router();
 router.post('/api/register', createUser);
+router.post('/api/login', userLogin);
+
 
 export default router;
